07538d5347bf3189917d8c525df04fe596328c02
zhangsongyang <328429080@qq.com>
2014-12-31 14:47:42 +0800

    测试还原

--- src/main/java/cn/zsy/util/App.java
+++ src/main/java/cn/zsy/util/App.java
@@ -6,15 +6,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("");
-
-//        ddd
-//
-        //
-        ////1
     }
 
-
-    public static void test(){
-
-    }
 }
