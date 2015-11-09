baf54d637d3fdbc614645acc560a69db24ebeaf4
328429080@qq.com <tcl581638ufo>
2015-11-09 20:05:53 +0800

    log4j修改

---
+++ src/main/java/cn/zsy/eg/slfFj/TestSlfFj.java
@@ -0,0 +1,19 @@
+package cn.zsy.eg.slfFj;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestSlfFj {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestSlfFj.class);
+
+    public static void main(String[] args) {
+        logger.debug("This is debug message");
+        logger.info("This is info message");
+        logger.warn("This is warn message");
+        logger.error("This is error message");
+        logger.info("{} is {}", new String[]{"x","y"});
+    }
+
+}
