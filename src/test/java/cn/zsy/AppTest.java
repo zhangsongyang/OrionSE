9e43b13ea2bb84bbd90d544aca7d3157e7fb388c
328429080@qq.com <tcl581638ufo>
2015-12-03 18:44:01 +0800

    slffj

--- src/test/java/cn/zsy/AppTest.java
+++ src/test/java/cn/zsy/AppTest.java
@@ -3,6 +3,13 @@ package cn.zsy;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -34,5 +41,26 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+
+    @org.junit.Test
+    public void ttt() throws Exception{
+//        File file = new File("E:\\Users\\zhangsongyang\\github\\OrionSE\\src\\main\\resources\\mac-2015-09-24.txt");
+//        List<String> list = FileUtils.readLines(file);
+//        File fileA = new File("E:\\Users\\zhangsongyang\\github\\OrionSE\\src\\main\\resources\\mac-cid-46-47.txt");
+        System.out.println("asdf");
+//        List<String> listA = FileUtils.readLines(fileA);
+//        System.out.println(listA.size());
+//        for(String str : listA){
+//            System.out.println(str);
+//        }
+
+        //\xb0\xae\xc6\xe6\xd2\xd5PPS\xd3\xb0\xd2\xf4%20(\xce\xb4\xcf\xec\xd3\xa6)
+
+        System.out.println(URLDecoder.decode("\\xb0\\xae\\xc6\\xe6\\xd2\\xd5PPS\\xd3\\xb0\\xd2\\xf4%20(\\xce\\xb4\\xcf\\xec\\xd3\\xa6", "utf-8"));
+        System.out.println(URLEncoder.encode("张"));
+
+
     }
 }
