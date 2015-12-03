c6650350a753f2c97f3ed319a08b18ef9c824454
328429080@qq.com <tcl581638ufo>
2015-12-03 18:41:47 +0800

    slffj

--- src/main/java/cn/zsy/eg/redis/RedisExecute.java
+++ src/main/java/cn/zsy/eg/redis/RedisExecute.java
@@ -1,15 +1,25 @@
 package cn.zsy.eg.redis;
 
+import org.apache.commons.lang.StringUtils;
 import redis.clients.jedis.Jedis;
+
+import java.util.Set;
 
 public class RedisExecute {
 
 
     public static void main(String[] args){
         Jedis jedis = new Jedis("127.0.0.1", 6379);
+        Set<String> keys = jedis.keys("*");
         jedis.set("family", "xshzsy");
         String value = jedis.get("family");
         System.out.println(value);
+        System.out.println(keys.size());
+        Object[] keyArray =  keys.toArray();
+        for(int i=0; i<keyArray.length; i++){
+            System.out.println(keyArray[i]);
+        }
+        jedis.close();
     }
 
 
