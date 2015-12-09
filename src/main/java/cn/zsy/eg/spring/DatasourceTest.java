fb1fae28470960b491a642e75411218468f7291a
328429080@qq.com <tcl581638ufo>
2015-12-09 17:59:08 +0800

    spring

--- src/main/java/cn/zsy/eg/spring/DatasourceTest.java
+++ src/main/java/cn/zsy/eg/spring/DatasourceTest.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatasourceTest {
 
@@ -13,5 +13,37 @@ public class DatasourceTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("datasorce-bean.xml");
         DataSource dataSource = context.getBean("comboPooledDataSource", ComboPooledDataSource.class);
         System.out.println("########> " + dataSource.getConnection());
+        Connection con = dataSource.getConnection();
+        String sql = "SELECT * from user";
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+        String name = null;
+        String pass = null;
+        while (rs.next()) {
+            name = rs.getString("name");
+            pass = rs.getString("passwd"); // 此方法比较高效
+            System.out.println(name + "   " + pass);
+        }
+        if (rs != null) {   // 关闭记录集
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (pstmt != null) {   // 关闭声明
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (con != null) {  // 关闭连接对象
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
