b94bf8ab6cb69e57bca7c919ce68b27f58f6121b
328429080@qq.com <tcl581638ufo>
2015-12-14 15:46:21 +0800

    lombok

--- src/main/java/cn/zsy/eg/lombok/TestMain.java
+++ src/main/java/cn/zsy/eg/lombok/TestMain.java
@@ -1,12 +1,20 @@
 package cn.zsy.eg.lombok;
 
 
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public class TestMain {
 
     public static void main(String[] args){
         UserBean userBean = new UserBean();
+        userBean.setName("asdfsadfsadf");
         System.out.println(userBean.toString());
+        UserBean userBean1 = new UserBean();
+        userBean1.setName("sdf");
+        UserBean u = new UserBean("asdf", 22, true);
+        System.out.println(userBean.canEqual(userBean1));
+        log.info("aSDFS");
     }
 
 }
--- src/main/java/cn/zsy/eg/lombok/UserBean.java
+++ src/main/java/cn/zsy/eg/lombok/UserBean.java
@@ -1,12 +1,24 @@
 package cn.zsy.eg.lombok;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-@RequiredArgsConstructor
+/*
+ *  @Data   ：注解在类上；提供类所有属性的 getting 和 setting 方法，此外还提供了equals、canEqual、hashCode、toString 方法
+ *  @Setter：注解在属性上；为属性提供 setting 方法
+ *  @Getter：注解在属性上；为属性提供 getting 方法
+ *  @Log4j ：注解在类上；为类提供一个 属性名为log 的 log4j 日志对象
+ *  @NoArgsConstructor：注解在类上；为类提供一个无参的构造方法
+ *  @AllArgsConstructor：注解在类上；为类提供一个全参的构造方法
+ */
+
+
+@Data
+@Log4j
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserBean {
 
     private String name;
