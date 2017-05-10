013fc075c9852a3afca878ab03171bfaa65532c1
张松杨 <zhangsongyang@thinkjoy.cn>
2017-05-10 09:24:02 +0800

    apacheCommon

--- src/main/java/cn/zsy/eg/ApacheCommon/SerializationUtilsTest.java
+++ src/main/java/cn/zsy/eg/ApacheCommon/SerializationUtilsTest.java
@@ -6,11 +6,15 @@ public class SerializationUtilsTest {
 
     public static void main(String[] args) {
         Teacher teacher = new Teacher();
-        teacher.setAge(27);
-        teacher.setName("zhangsongyang");
-        byte[] b = SerializationUtils.serialize(teacher);     //序列化
+        byte[] b = getBytes(teacher);
         Teacher teacher1 = SerializationUtils.deserialize(b); //反序列化
         System.out.println("Serialize: " + b.toString() + "\ndeSerialization: " + teacher1);
+    }
+
+    private static byte[] getBytes(Teacher teacher) {
+        teacher.setAge(27);
+        teacher.setName("zhangsongyang");
+        return SerializationUtils.serialize(teacher);
     }
 
 }
