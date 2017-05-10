120852f699144ac7ea703380413b76dffb96e2eb
张松杨 <zhangsongyang@thinkjoy.cn>
2017-05-10 09:36:45 +0800

    spring

--- src/main/java/cn/zsy/eg/spring/beans/Person.java
+++ src/main/java/cn/zsy/eg/spring/beans/Person.java
@@ -41,4 +41,9 @@ public class Person {
                 ", car=" + car +
                 '}';
     }
+
+    public void init(){
+        System.out.println("init............."+toString());
+    }
+
 }
--- src/main/java/cn/zsy/eg/spring/controller/Main.java
+++ src/main/java/cn/zsy/eg/spring/controller/Main.java
@@ -6,8 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
+    public static final String SPRINT_ANNOTATION_XML = "sprint-annotation.xml";
+
     public static void main(String[] args){
-        ApplicationContext ac = new ClassPathXmlApplicationContext("sprint-annotation.xml");
+        ApplicationContext ac = new ClassPathXmlApplicationContext(SPRINT_ANNOTATION_XML);
         BaseController baseController = ac.getBean("baseController", BaseController.class);
         baseController.saveController();
     }
